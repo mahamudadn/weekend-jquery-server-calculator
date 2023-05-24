a49656e5056e7adb4a27341e16945efe64167f7c
Mahamud Aden <mahamudadn@gmail.com>
2023-05-24 10:58:44 -0500

    indented my code for readibility

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,91 +1,91 @@
-$(document).ready(onReady);
+    $(document).ready(onReady);
 
- // listen for form submit event
-function onReady() {
-    console.log('jquery is working')
+    // listen for form submit event
+    function onReady() {
+        console.log('jquery is working')
 
-    $('#formValue').on('submit' ,sendCalculation)
- // listen for operator button click event
-    $('.calculator').on('click', callOperator)
-// listen for clear button click event
-    $('#clear').on('click', clearinput)
+        $('#formValue').on('submit' ,sendCalculation)
+    // listen for operator button click event
+        $('.calculator').on('click', callOperator)
+    // listen for clear button click event
+        $('#clear').on('click', clearinput)
 
-// fetch the calculation history from the server
-    getCal();
-}
-
-// defined a global variable to store the selected operator
-let operator ;
-
-// event handler for operator button click
-function callOperator  (event) {
-    event.preventDefault();
- // store the selected operator in the global variable
-    operator = $(this).data().value
-    console.log('See our posted', operator)
-}
-// event handler for clear button click
-function clearinput() {
-    $('#Number-One').val('')
-    $('#Number-Two').val('')
-}
-// event handler for form submit
-function sendCalculation (event) {
-        event.preventDefault();
-    console.log('sendcalt working');
-
-    console.log('see our posted operator')
- // retrieve the user inputs
-    let inputOne = $('#Number-One').val()
-    let inputTwo = $('#Number-Two').val()
- // send a POST request to the server to save the calculation
-    $.ajax({
-        method: 'POST',
-        url:'/history',
-        data: {
-            inputOne, 
-            inputTwo,
-            operator
-        
-        }
-
-    }).then(function(response ) {
-// fetch the updated calculation history from the server
+    // fetch the calculation history from the server
         getCal();
-    })
-    
-}
+    }
 
-// fetches the calculation history from the server
-function getCal() {
-    console.log('getcalt working')
-    $.ajax({
-        method:'GET',
-        url:'/history',
-    }).then(function (response) {
-        renderToDom(response);
-        console.log('Our Get Works', response)
+    // defined a global variable to store the selected operator
+    let operator ;
 
+    // event handler for operator button click
+    function callOperator  (event) {
+        event.preventDefault();
+    // store the selected operator in the global variable
+        operator = $(this).data().value
+        console.log('See our posted', operator)
+    }
+    // event handler for clear button click
+    function clearinput() {
+        $('#Number-One').val('')
+        $('#Number-Two').val('')
+    }
+    // event handler for form submit
+    function sendCalculation (event) {
+            event.preventDefault();
+        console.log('sendcalt working');
+
+        console.log('see our posted operator')
+    // retrieve the user inputs
+        let inputOne = $('#Number-One').val()
+        let inputTwo = $('#Number-Two').val()
+    // send a POST request to the server to save the calculation
+        $.ajax({
+            method: 'POST',
+            url:'/history',
+            data: {
+                inputOne, 
+                inputTwo,
+                operator
+            
+            }
+
+        }).then(function(response ) {
+    // fetch the updated calculation history from the server
+            getCal();
+        })
         
-    })
-}
+    }
+
+    // fetches the calculation history from the server
+    function getCal() {
+        console.log('getcalt working')
+        $.ajax({
+            method:'GET',
+            url:'/history',
+        }).then(function (response) {
+            renderToDom(response);
+            console.log('Our Get Works', response)
+
+            
+        })
+    }
 
 
-// renders the calculation history to the DOM
-function renderToDom(arrays) {
-    $('#solution').empty();
-    $('#history').empty();
+    // renders the calculation history to the DOM
+    function renderToDom(arrays) {
+        $('#solution').empty();
+        $('#history').empty();
 
-//    let history = array[array.length -1]
-for( array of arrays) {
-    $('#solution').text(array.total)
-    $('#history').append(`
-    <li> ${array.inputOne} ${array.operator} ${array.inputTwo} = ${array.total} </li>
+    //    let history = array[array.length -1]
+    for( array of arrays) {
+        $('#solution').text(array.total)
+        $('#history').append(`
+        <li> ${array.inputOne} ${array.operator} ${array.inputTwo} = ${array.total} </li>
 
-    `)
-}
-    
+        `)
+    }
+        
 
-}
+    }
 
 
--- server/server.js
+++ server/server.js
@@ -1,55 +1,55 @@
-// bring express into your project
-const express = require('express');
+      // bring express into your project
+      const express = require('express');
 
-const bodyParser = require('body-parser')
+      const bodyParser = require('body-parser')
 
-// identify which port we'll run on
-const port = process.env.PORT||5000;
-// bring express into your project
-let history = [];
-let solution = 0;
+      // identify which port we'll run on
+      const port = process.env.PORT||5000;
+      // bring express into your project
+      let history = [];
+      let solution = 0;
 
-const app = express();
-// serve static files from the "public" directory
-app.use(express.static('server/public'))
-// parse URL-encoded form data and store it in req.body
-app.use(bodyParser.urlencoded({extended:true}))
+      const app = express();
+      // serve static files from the "public" directory
+      app.use(express.static('server/public'))
+      // parse URL-encoded form data and store it in req.body
+      app.use(bodyParser.urlencoded({extended:true}))
 
 
-// app post that will store my client post
-app.post('/history',function(req, res) {
- // extract input values from the request body
-let inputs = req.body
+      // app post that will store my client post
+      app.post('/history',function(req, res) {
+      // extract input values from the request body
+      let inputs = req.body
 
-  console.log('got the history', inputs)
-// calculate the total using the input values and operator
-  let total = Solution((inputs.inputOne*1), (inputs.inputTwo*1), inputs.operator)
-  console.log('got the history', total)
-// push a new object with input values, operator, and total to the history array
-  history.push({inputOne:inputs.inputOne, inputTwo:inputs.inputTwo, operator:inputs.operator, total})
-  res.sendStatus(201)
-})  
-// app get that will get my client history data.
-app.get('/history', function(req, res){
-  res.send(history)
-})
+        console.log('got the history', inputs)
+      // calculate the total using the input values and operator
+        let total = Solution((inputs.inputOne*1), (inputs.inputTwo*1), inputs.operator)
+        console.log('got the history', total)
+      // push a new object with input values, operator, and total to the history array
+        history.push({inputOne:inputs.inputOne, inputTwo:inputs.inputTwo, operator:inputs.operator, total})
+        res.sendStatus(201)
+      })  
+      // app get that will get my client history data.
+      app.get('/history', function(req, res){
+        res.send(history)
+      })
 
-// function that that calculates the my inputs and return total.
-function Solution(inputOne, inputTwo, output) {
-  let total = 0;
-if('+' === output) {
-  total = inputOne +inputTwo
-} else if ('-' === output) {
-  total =  inputOne - inputTwo
-} else if ('*' === output) {
-  total = inputOne * inputTwo
-} else if ('/' === output) {
-  total = inputOne / inputTwo
-}
-  return total
-    }
-  
- // start listening for incoming requests on the specified port
-app.listen(port, () => {
-    console.log('listening on port', port);
-  })
+      // function that that calculates the my inputs and return total.
+      function Solution(inputOne, inputTwo, output) {
+        let total = 0;
+      if('+' === output) {
+        total = inputOne +inputTwo
+      } else if ('-' === output) {
+        total =  inputOne - inputTwo
+      } else if ('*' === output) {
+        total = inputOne * inputTwo
+      } else if ('/' === output) {
+        total = inputOne / inputTwo
+      }
+        return total
+          }
+        
+      // start listening for incoming requests on the specified port
+      app.listen(port, () => {
+          console.log('listening on port', port);
+        })
